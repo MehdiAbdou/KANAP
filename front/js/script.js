c0630db5908199592167c9c202fda4477a46b59e
MehdiAbdou <mehdi.abdou96@gmail.com>
2022-06-09 20:49:39 +0200

    Recupération Api + Boucle de creation card

--- front/js/script.js
+++ front/js/script.js
@@ -11,6 +11,7 @@ fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((apiProduit) => {
         console.table(apiProduit);
+        cardKanap(apiProduit);
     })
 /* 
 Si l'Api n'est pas trouvé, le texte associé à la classe .titles se transforme en H1 d'erreur 
@@ -20,5 +21,30 @@ et la console affiche une erreur d'Api non trouvée
         document.querySelector(".titles").innerHTML = "<h1>Erreur</h1>";
         console.log("'API not found':" + erreur);
     });
+
+
+// Fonction d'affichage des cards articles (index correspond à la première valeur du tableau)
+
+/* 
+Déclaration de la variable sur le selecteur html
+Boucle pour automatisé la creation de card pour chaque article les unes après les autres
+Récupération des paramètres de l'url afin de savoir quel produit de l'api afficher sur la page produit grace à l'id
+Code relatif à l'ajout de contenu html
+*/
+
+function cardKanap(card) {
+    let zoneCard = document.querySelector("#items");
+    for (let article of card) {
+
+      zoneCard.innerHTML += `<a href="./product.html?_id=${article._id}">
+
+      <article>
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+        <h3 class="productName">${article.name}</h3>
+        <p class="productDescription">${article.description}</p>
+      </article>
+    </a>`;
+    }
+  }
 
     
