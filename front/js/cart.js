32b71d87a1b7d60cd5913436b04d18c383c60ebe
MehdiAbdou <mehdi.abdou96@gmail.com>
2022-07-08 19:46:16 +0200

    Fin display cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,81 +3,81 @@ fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((apiProduit) => {
         cartKanap(apiProduit);
-        console.log(apiProduit)
+
     })
  
 
 
-//Récupération du localStorage
-let getCart = JSON.parse(localStorage.getItem("cart"));
-
-console.log(getCart)
-
-//Pointeur html 
-let zonePanier = document.querySelector("#cart__items");
-
- 
-
 function cartKanap(index){
     //Si le panier est vide, alors on affiche Panier vide
+    let getCart = JSON.parse(localStorage.getItem("cart"));
+    console.log("cart" , getCart);
+      console.log("index", index);
     if(getCart == null){
-let emptyCart = `
-<div class="cartAndFormContainer">
-<h1> est vide <h1>
-<div>
-`
-zonePanier.innerHTML = emptyCart
+      let emptyCart = `
+      <div class="cartAndFormContainer">
+      <h1> est vide <h1>
+      <div>
+      `
+      zonePanier.innerHTML = emptyCart
     }else{
         //Sinon on cherche une correspondance clef/valeur de l'api et du panier et on crée les valeurs à afficher
           for (let choix of getCart) {
-            for (let j = 0, k = index.length; j < k; j++) {
-                if (choix.productId === index[j]._id) {
-                    choix.name = index[j].name;
-                    choix.prix = index[j].price;
-                    choix.image = index[j].imageUrl;
-                    choix.description = index[j].description;
-                    choix.alt = index[j].altTxt;
-                }
+            produit = index.find(param => param.productId == choix._id);
+            console.log("prduit", produit)
+            if(produit != undefined) {
+                choix.name = produit.name;
+                choix.prix = produit.price;
+                choix.image = produit.imageUrl;
+                choix.description = produit.description;
+                choix.alt = produit.altTxt;
+
+                //On passe getCart à la fonction d'affichage
+              affiche(choix);
             }
-          }
-          //On passe getCart à la fonction d'affichage
-          affiche(getCart);
+            
+          } 
     }
 }
 
-function affiche(indexé) {
+function affiche(choix) {
+      //Pointeur html 
+    let zonePanier = document.querySelector("#cart__items");
     /// Déclaration variable a injecter dans le inner.html
-    
-   let displayPanier = indexé.map((choix) => 
+    console.log("affiche", choix);
+  
+   let displayPanier = 
     `
-    <article class="cart__item" data-id="${choix._id}" data-couleur="${choix.couleur}" data-quantité="${choix.quantité}"> 
+    <article class="cart__item" data-id="${choix._id}" data-couleur="${choix.color}" data-quantité="${choix.quantity}"> 
     <div class="cart__item__img">
       <img src="${choix.image}" alt="${choix.alt}">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__titlePrice">
         <h2>${choix.name}</h2>
-        <span>couleur : ${choix.couleur}</span>
+        <span>couleur : ${choix.color}</span>
         <p data-prix="${choix.prix}">${choix.prix} €</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${choix.quantité}">
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${choix.quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
-          <p class="deleteItem" data-id="${choix._id}" data-couleur="${choix.couleur}">Supprimer</p>
+          <p class="deleteItem" data-id="${choix._id}" data-couleur="${choix.color}">Supprimer</p>
         </div>
       </div>
     </div>
   </article>
-  `
-  ).join("");
-  
-    zonePanier.innerHTML += displayPanier 
+  `;
+    zonePanier.innerHTML += displayPanier ;
     
   }
 
+// Fonction de sauvegarde du panier dans le local storage
+function saveCart(items) {
+  localStorage.setItem("cart", JSON.stringify(items))
+}
 
 
   
